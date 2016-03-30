b75e714e702b47dc760abdc5a78aeb7a67842df9
ChristianDelg <ing.delgadomontiel@gmail.com>
2016-03-30 10:55:28 -0400

    Cambio en el archivo models.py asigne mas espacio pregunta
    Signed-off-by: ChristianDelg <ing.delgadomontiel@gmail.com>

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -19,7 +19,7 @@ class Question(models.Model):
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=500)
     votes = models.IntegerField(default=0)
     @python_2_unicode_compatible
     def __str__(self):
