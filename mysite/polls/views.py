fd58e022873ef8d1d3e2a9e495f645c9435e9c6b
Ferbri <ferbri10@gmail.com>
2016-03-30 12:25:44 -0400

    Mayuscula Puse en mayuscula question Signed-off-by: Ferbri <ferbri10@gmail.com>

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -21,4 +21,4 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    return HttpResponse("You are voting on QUESTION %s." % question_id)
